aac609aefd9046e28d20d3fbd53f6fbc29b14459
Donna Amos <donnacamos.developer@gmail.com>
2019-02-07 02:27:12 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,11 +1,11 @@
 var chocolateBars = [ "snickers", "hundred grand", "kitkat", "skittles"]
 
 function addElementToBeginningOfArray(){
-  chocolateBars.unshift("milky way")
+  ["milky way", ...chocolateBars]
 }
 
 function destructivelyAddElementToBeginningOfArray(){
-  ["milky way", ...chocolateBars]
+  [...chocolateBars, "milky way"]
 }
 
 function addElementToEndOfArray(){
